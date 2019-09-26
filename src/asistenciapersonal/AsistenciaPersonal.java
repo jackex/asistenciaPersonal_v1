70230af566167a2c49705d533eaf3eff6ec47a76
julian certuche <julian.certuche.c@gmail.com>
2019-09-25 23:39:02 -0500

    se actualiza el formato para mostrar la hora exacta junto con el nombre del empleado

--- src/asistenciapersonal/AsistenciaPersonal.java
+++ src/asistenciapersonal/AsistenciaPersonal.java
@@ -5,6 +5,8 @@
  */
 package asistenciapersonal;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author juliancc
@@ -16,6 +18,48 @@ public class AsistenciaPersonal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArrayList<String> lista = new ArrayList<>();
+
+        lista.add("03:59:58");
+        lista.add("01:49:54");
+        lista.add("01:02:04");
+        lista.add("01:36:05");
+        lista.add("14:47:05");
+        lista.add("07:18:50");
+        lista.add("02:00:20");
+        lista.add("00:00:09");
+        lista.add("03:04:09");
+        lista.add("00:04:01");
+        lista.add("00:18:01");
+
+        int horas = 0;
+        int minutos = 0;
+        int segundos = 0;
+        for (int x = 0; x < lista.size(); x++) {
+            String[] array = lista.get(x).split(":");
+            horas += Integer.parseInt(array[0]);
+            minutos += Integer.parseInt(array[1]);
+            segundos += Integer.parseInt(array[2]);
+        }
+
+        int tmpsegundos = 0;
+        tmpsegundos = segundos;
+        while (tmpsegundos >= 60) {
+                minutos += (tmpsegundos / 60);
+                tmpsegundos = (tmpsegundos % 60);
+        }
+        segundos = tmpsegundos;
+        
+        int tmpminutos = 0;
+        tmpminutos = minutos;
+        while(tmpminutos > 60){
+            horas += (tmpminutos / 60);
+            tmpminutos = (tmpminutos % 60);
+        }
+        minutos = tmpminutos;
+
+        String tiempoTotal = horas + ":" + minutos + ":" + segundos;
+        System.out.println(tiempoTotal);
     }
-    
+
 }
--- src/control/GenerarExcel.java
+++ src/control/GenerarExcel.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.BorderStyle;
@@ -41,11 +42,28 @@ public class GenerarExcel {
     }
 
     public void crearExcel(String documento) throws SQLException {
-        String tiempoTotal = "";
         Workbook book = new HSSFWorkbook();
-        Sheet sheet = book.createSheet("HORAS DE ASISTENCIA");
+        Sheet sheet = book.createSheet("REPORTE DE ASISTENCIA");
         try {
-
+            String nombreCompleto = "";
+            String identificacion = "";
+            String cargo = "";
+            String municipio  = "";
+            String consulta = "SELECT DISTINCT PRIMER_NOMBRE,SEGUNDO_NOMBRE,PRIMER_APELLIDO, SEGUNDO_APELLIDO, DOCUMENTO, "
+                    + "CARGOS.CARGO, MUNICIPIOS.NOMBRE "
+                    + "FROM EMPLEADOS INNER JOIN CARGOS ON EMPLEADOS.FK_CARGO = CARGOS.ID  "
+                    + "INNER JOIN MUNICIPIOS ON EMPLEADOS.FK_MUNICIPIO = MUNICIPIOS.ID "
+                    + "INNER JOIN HORA_INGRESO ON HORA_INGRESO.FK_EMPLEADO = EMPLEADOS.ID AND EMPLEADOS.DOCUMENTO = ?";
+            PreparedStatement PS2 = conexion.getConnection().prepareStatement(consulta);
+            PS2.setString(1,documento);
+            ResultSet RS2 = PS2.executeQuery();
+            while(RS2.next()){
+                nombreCompleto = RS2.getString("PRIMER_NOMBRE")+ " "+RS2.getString("SEGUNDO_NOMBRE")+ " " +RS2.getString("PRIMER_APELLIDO")+ " "+RS2.getString("SEGUNDO_APELLIDO");
+                identificacion = RS2.getString("DOCUMENTO");
+                cargo = RS2.getString("CARGOS.CARGO");
+                municipio = RS2.getString("MUNICIPIOS.NOMBRE");
+            }  
+            
             CellStyle estiloTitulo = book.createCellStyle();
             estiloTitulo.setAlignment(HorizontalAlignment.CENTER);
             estiloTitulo.setVerticalAlignment(VerticalAlignment.CENTER);
@@ -61,12 +79,12 @@ public class GenerarExcel {
             celdaTitulo.setCellValue("FORMATO DE REPORTE PARA IMPRESION DEL NÚMERO DE HORAS PRESTADAS"
                     + " POR EMPLEADO");
 
-            sheet.addMergedRegion(new CellRangeAddress(1, 1, 1, 9));
-            String[] contenidoTitulo = new String[]{"HORA DE INGRESO", "HORA DE SALIDA", "HORAS TRABAJADAS", "COMENTARIOS"};
+            sheet.addMergedRegion(new CellRangeAddress(1, 1, 1, 5));
+            String[] contenidoTitulo = new String[]{"FECHA DE INGRESO","HORA DE INGRESO","FECHA SALIDA", "HORA DE SALIDA", "HORAS TRABAJADAS", "COMENTARIOS"};
 
             CellStyle headerStyle = book.createCellStyle();
             headerStyle.setAlignment(HorizontalAlignment.CENTER);
-            headerStyle.setFillForegroundColor(IndexedColors.OLIVE_GREEN.getIndex());
+            headerStyle.setFillForegroundColor(IndexedColors.TEAL.getIndex());
             headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
             headerStyle.setBorderTop(BorderStyle.THIN);
             headerStyle.setBorderRight(BorderStyle.THIN);
@@ -86,8 +104,37 @@ public class GenerarExcel {
             datosEstilo.setBorderRight(BorderStyle.THIN);
             datosEstilo.setBorderBottom(BorderStyle.THIN);
             datosEstilo.setBorderLeft(BorderStyle.THIN);
+            
+            Row filaDatosPersonales = sheet.createRow(3);
+            
+            Cell celdaDatosPersonales = filaDatosPersonales.createCell(0);
+            celdaDatosPersonales.setCellStyle(headerStyle);
+            celdaDatosPersonales.setCellValue("NOMBRE EMPLEADO");
+            celdaDatosPersonales =  filaDatosPersonales.createCell(1);
+            celdaDatosPersonales.setCellValue(nombreCompleto);
+            
+            filaDatosPersonales = sheet.createRow(4);
+            celdaDatosPersonales = filaDatosPersonales.createCell(0);
+            celdaDatosPersonales.setCellStyle(headerStyle);
+            celdaDatosPersonales.setCellValue("DOCUMENTO");
+            celdaDatosPersonales =  filaDatosPersonales.createCell(1);
+            celdaDatosPersonales.setCellValue(documento);
+            
+            filaDatosPersonales = sheet.createRow(5);
+            celdaDatosPersonales = filaDatosPersonales.createCell(0);
+            celdaDatosPersonales.setCellStyle(headerStyle);
+            celdaDatosPersonales.setCellValue("CARGO");
+            celdaDatosPersonales =  filaDatosPersonales.createCell(1);
+            celdaDatosPersonales.setCellValue(cargo);
+            
+            filaDatosPersonales = sheet.createRow(6);
+            celdaDatosPersonales = filaDatosPersonales.createCell(0);
+            celdaDatosPersonales.setCellStyle(headerStyle);
+            celdaDatosPersonales.setCellValue("MUNICIPIO");
+            celdaDatosPersonales =  filaDatosPersonales.createCell(1);
+            celdaDatosPersonales.setCellValue(municipio);
 
-            Row filaEncabezado = sheet.createRow(5);
+            Row filaEncabezado = sheet.createRow(8);
 
             for (int i = 0; i < contenidoTitulo.length; i++) {
                 Cell celdaEncabezado = filaEncabezado.createCell(i);
@@ -95,46 +142,64 @@ public class GenerarExcel {
                 celdaEncabezado.setCellValue(contenidoTitulo[i]);
             }
 
-            String query = "SELECT HORA_INGRESO_AM, HORA_SALIDA_PM,HORAS_TRABAJADAS_PM,COMENTARIOS_PM "
-                    + "FROM HORA_INGRESO INNER JOIN EMPLEADOS ON EMPLEADOS.ID = HORA_INGRESO.FK_EMPLEADO AND EMPLEADOS.DOCUMENTO = ?";
-            String query2 = "SELECT CAST(CONVERT(SUM(HORA_INGRESO.HORAS_TRABAJADAS_PM),TIME) AS CHAR(20)) AS \"TIEMPO\" "
-                    + "FROM HORA_INGRESO INNER JOIN EMPLEADOS ON EMPLEADOS.ID = HORA_INGRESO.FK_EMPLEADO AND EMPLEADOS.DOCUMENTO = ?";
-
+            String query = "SELECT DATE_FORMAT(HORA_INGRESO_AM,\"%Y-%m-%d\") ,DATE_FORMAT(HORA_INGRESO_AM,\"%H:%i:%s\") , "
+                    + "DATE_FORMAT(HORA_SALIDA_PM,\"%Y-%m-%d\"),DATE_FORMAT(HORA_SALIDA_PM,\"%H:%i:%s\"),HORAS_TRABAJADAS_PM ,"
+                    + "COMENTARIOS_PM FROM HORA_INGRESO "
+                    + "INNER JOIN EMPLEADOS ON EMPLEADOS.ID = HORA_INGRESO.FK_EMPLEADO AND EMPLEADOS.DOCUMENTO = ?";
+            
+            ArrayList<String> lista = new ArrayList<>();
             PreparedStatement PS;
             PS = conexion.getConnection().prepareStatement(query);
             PS.setString(1, documento);
             ResultSet RS = PS.executeQuery();
             int numCols = RS.getMetaData().getColumnCount();
-            int filaDatos = 6;
+            int filaDatos = 9;
             while (RS.next()) {
                 Row FilaDatos = sheet.createRow(filaDatos);
                 for (int a = 0; a < numCols; a++) {
                     Cell celdaDatos = FilaDatos.createCell(a);
                     celdaDatos.setCellStyle(datosEstilo);
-                    if (a == 2) {
+                    if (a == 4) {
                         celdaDatos.setCellValue(RS.getTime(a + 1));
-//                        tiempo.add(RS.getString(a + 1));
-//                        sumarTiempo += Integer.parseInt(RS.getString(a + 1));
+                        lista.add(RS.getString(a + 1));
                     }
                     celdaDatos.setCellValue(RS.getString(a + 1));
                 }
                 filaDatos++;
             }
-            PreparedStatement PS2;
-            PS2 = conexion.getConnection().prepareStatement(query2);
-            PS2.setString(1, documento);
-            ResultSet RS2 = PS2.executeQuery();
-            while (RS2.next()) {
-                tiempoTotal = RS2.getString("TIEMPO");
+
+            int horas = 0;
+            int minutos = 0;
+            int segundos = 0;
+            for (int x = 0; x < lista.size(); x++) {
+                String[] array = lista.get(x).split(":");
+                horas += Integer.parseInt(array[0]);
+                minutos += Integer.parseInt(array[1]);
+                segundos += Integer.parseInt(array[2]);
             }
+
+            int tmpsegundos = segundos;
+            while (tmpsegundos >= 60) {
+                minutos += (tmpsegundos / 60);
+                tmpsegundos = (tmpsegundos % 60);
+            }
+            segundos = tmpsegundos;
+
+            int tmpminutos = minutos;
+            while (tmpminutos > 60) {
+                horas += (tmpminutos / 60);
+                tmpminutos = (tmpminutos % 60);
+            }
+            minutos = tmpminutos;
+            String tiempoTotal = horas + ":" + minutos + ":" + segundos;
 
             Row FilaDatos = sheet.createRow(filaDatos);
             Cell celdaDatos = FilaDatos.createCell(0);
-            celdaDatos.setCellStyle(datosEstilo);
-            celdaDatos.setCellValue("TOTAL HORAS");
-            Cell celdaDatos2 = FilaDatos.createCell(2);
+            celdaDatos.setCellStyle(headerStyle);
+            celdaDatos.setCellValue("TOTAL HORAS TRABAJADAS");
+            Cell celdaDatos2 = FilaDatos.createCell(1);
             celdaDatos2.setCellStyle(datosEstilo);
-            celdaDatos2.setCellValue(tiempoTotal.toString());
+            celdaDatos2.setCellValue(tiempoTotal);
 
             for (int x = 0; x < numCols; x++) {
                 sheet.autoSizeColumn(x);
